19ee6f0a0a770d69bf3a3001bdd2717e61a0380f
cobbygraves <rmeocnis@hotmail.com>
2024-11-24 04:34:42 +0000

    increase mobile sheet height

--- src/app/components/MobileFilter.tsx
+++ src/app/components/MobileFilter.tsx
@@ -39,7 +39,7 @@ export default function MobileFilter() {
         </SheetTrigger>
         <SheetContent
           side='bottom'
-          className=' rounded-t-xl bg-hostel-yellow h-[60vh]'
+          className=' rounded-t-xl bg-hostel-yellow h-[75vh]'
         >
           <SheetHeader>
             <SheetTitle>
