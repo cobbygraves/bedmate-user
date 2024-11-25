0354f18de0f2f0fc269e5f3eb058db41602ced70
cobbygraves <rmeocnis@hotmail.com>
2024-11-25 14:11:16 +0000

    login and signin ui fixes

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -120,7 +120,7 @@ const SignIn = () => {
             </button>
 
             {/* Sign Up Link */}
-            <p className='text-center text-gray-600 text-sm mt-2'>
+            <p className='text-end text-gray-600 text-sm'>
               Don&apos;t have an account?{' '}
               <button
                 onClick={() => setSignup(true)}
