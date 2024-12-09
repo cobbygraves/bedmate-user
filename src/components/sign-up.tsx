8b3dda9779ddd9fe1e67876fc3fd0d79a990c781
cobbygraves <rmeocnis@hotmail.com>
2024-12-09 15:59:48 +0000

    fix error message display information

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -40,11 +40,13 @@ const SignUp = ({
       email
     }
     const userData = await userSignUp(data)
-    if (userData) {
+    console.log(userData)
+    if (userData?.response?.data?.error) {
+      setError(userData.response.data.error)
+    } else {
+      // setError(userData?.response?.data?.error)
       onSignIn()
       onSignUp()
-    } else {
-      setError(userData?.response?.data?.message)
     }
     // console.log(userData)
     setLoading(false)
