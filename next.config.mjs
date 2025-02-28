e26023f6b5ca225b1485e882f50aaeafba6adaa7
cobbygraves <rmeocnis@hotmail.com>
2025-02-28 05:19:26 +0000

    configure next-config to accept images from aws

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'bed-mate.s3.af-south-1.amazonaws.com',
         port: '',
         pathname: '/**'
       }
